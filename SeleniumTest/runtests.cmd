b31c27e5599ff3fd35f6adaf728a3cf6ca431ec5
DESKTOP-7MQ34CQ\Nataliia <nzhenchak@infusion.com>
2018-01-31 22:33:59 +0100

    Runtest finished

--- SeleniumTest/runtests.cmd
+++ SeleniumTest/runtests.cmd
@@ -1,15 +1,20 @@
 @pushd %~dp0
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" ""
-
+"%MSBuild_PATH%MSBuild.exe" ""
+echo lala
+echo %MSBuild_PATH%
 @if ERRORLEVEL 1 goto end
 
 @cd ..\packages\SpecRun.Runner.*\tools
 
-@set profile=%1
+@set profile=%2
+@set browser=%1
+
+@if "%browser%" == "" set browser=Chrome
 @if "%profile%" == "" set profile=Default
 
-SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\FireFox" /log:specrun.log %2 %3 %4 %5
+
+SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\%browser%" /log:specrun.log %3 %4 %5 %6
 
 :end
 
